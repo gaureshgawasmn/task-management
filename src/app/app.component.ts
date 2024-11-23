709e5e73e61195690ad5fb6c89c5d617cea5b7a2
gaureshgawasmn <gaureshgawasmn@gmail.com>
2024-11-23 15:05:51 +0530

    ability to see the profile of the user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { UsersComponent } from './users/users.component';
+import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "./header/header.component";
+import { User } from "./users/user/user.model";
+import { UsersComponent } from "./users/users.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [HeaderComponent, UsersComponent,RouterOutlet],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
+  imports: [HeaderComponent, UsersComponent, RouterOutlet],
 })
-export class AppComponent {}
+export class AppComponent {
+  selectedImage: string | undefined = undefined;
+
+  onProfileClose() {
+    this.selectedImage = undefined;
+  }
+
+  onProfileSelect(user: User) {
+    this.selectedImage = "/users/" + user.avatar;
+  }
+}
